08e70ed882a6bc72552964daf73998844c92669f
Lukáš Kníže <82168954+LukasKnize@users.noreply.github.com>
2022-04-19 13:28:55 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/FromAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class FromAPI
+    {
+        public FromAPI(string name, string url)
+        {
+            Url = url;
+        }
+        //Your Properties are auto-implemented.
+        public string Url { get; set; }
+    }
+}
