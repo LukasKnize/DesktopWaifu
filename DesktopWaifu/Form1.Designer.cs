e4ecc53247e31419a589f6ddc99b4e2cfdae8167
Lukáš Kníže <82168954+LukasKnize@users.noreply.github.com>
2022-05-06 09:21:02 +0200

    updated UI

---
+++ DesktopWaifu/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace DesktopWaifu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayField = new System.Windows.Forms.PictureBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.TextField = new System.Windows.Forms.RichTextBox();
+            this.EnterField = new System.Windows.Forms.TextBox();
+            this.Previous = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.navBar = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayField)).BeginInit();
+            this.navBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DisplayField
+            // 
+            this.DisplayField.Location = new System.Drawing.Point(12, 45);
+            this.DisplayField.Name = "DisplayField";
+            this.DisplayField.Size = new System.Drawing.Size(374, 284);
+            this.DisplayField.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.DisplayField.TabIndex = 0;
+            this.DisplayField.TabStop = false;
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.Submit.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Submit.FlatAppearance.BorderSize = 0;
+            this.Submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Submit.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Submit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
+            this.Submit.Location = new System.Drawing.Point(820, 334);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(90, 24);
+            this.Submit.TabIndex = 1;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // TextField
+            // 
+            this.TextField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.TextField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
+            this.TextField.Location = new System.Drawing.Point(429, 45);
+            this.TextField.Name = "TextField";
+            this.TextField.ReadOnly = true;
+            this.TextField.Size = new System.Drawing.Size(481, 284);
+            this.TextField.TabIndex = 2;
+            this.TextField.Text = "";
+            // 
+            // EnterField
+            // 
+            this.EnterField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.EnterField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.EnterField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
+            this.EnterField.Location = new System.Drawing.Point(429, 340);
+            this.EnterField.Name = "EnterField";
+            this.EnterField.Size = new System.Drawing.Size(380, 13);
+            this.EnterField.TabIndex = 3;
+            this.EnterField.KeyDown += new System.Windows.Forms.KeyEventHandler(this.EnterField_KeyDown);
+            // 
+            // Previous
+            // 
+            this.Previous.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.Previous.FlatAppearance.BorderSize = 0;
+            this.Previous.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Previous.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
+            this.Previous.Location = new System.Drawing.Point(12, 334);
+            this.Previous.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Previous.Name = "Previous";
+            this.Previous.Size = new System.Drawing.Size(124, 25);
+            this.Previous.TabIndex = 4;
+            this.Previous.Text = "<";
+            this.Previous.UseVisualStyleBackColor = false;
+            this.Previous.Click += new System.EventHandler(this.Previous_Click);
+            // 
+            // Next
+            // 
+            this.Next.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.Next.FlatAppearance.BorderSize = 0;
+            this.Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Next.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
+            this.Next.Location = new System.Drawing.Point(262, 334);
+            this.Next.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(124, 25);
+            this.Next.TabIndex = 5;
+            this.Next.Text = ">";
+            this.Next.UseVisualStyleBackColor = false;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.save.FlatAppearance.BorderSize = 0;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
+            this.save.Location = new System.Drawing.Point(153, 334);
+            this.save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(92, 25);
+            this.save.TabIndex = 6;
+            this.save.Text = "save image";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // navBar
+            // 
+            this.navBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.navBar.Controls.Add(this.minimizeButton);
+            this.navBar.Controls.Add(this.closeButton);
+            this.navBar.Location = new System.Drawing.Point(0, -1);
+            this.navBar.Name = "navBar";
+            this.navBar.Size = new System.Drawing.Size(926, 40);
+            this.navBar.TabIndex = 7;
+            this.navBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NavBar_MouseDown);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Location = new System.Drawing.Point(893, 13);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(17, 17);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
+            this.minimizeButton.FlatAppearance.BorderSize = 0;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.Location = new System.Drawing.Point(870, 13);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(17, 17);
+            this.minimizeButton.TabIndex = 1;
+            this.minimizeButton.UseVisualStyleBackColor = false;
+            this.minimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(922, 385);
+            this.Controls.Add(this.navBar);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.Previous);
+            this.Controls.Add(this.EnterField);
+            this.Controls.Add(this.TextField);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.DisplayField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Desktop Waifu";
+            this.TransparencyKey = System.Drawing.Color.GhostWhite;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayField)).EndInit();
+            this.navBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DisplayField;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.RichTextBox TextField;
+        private System.Windows.Forms.TextBox EnterField;
+        private System.Windows.Forms.Button Previous;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Panel navBar;
+        private System.Windows.Forms.Button minimizeButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ DesktopWaifu/JSONClasses.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopWaifu {
+    /* #########################
+     * 
+     *   JESTLI TOHLE FUNGUJE
+     *   NEZAHEJTE NA TO
+     * 
+     * #########################*/
+
+
+    public class Jpg {
+        public string image_url { get; set; }
+        public string small_image_url { get; set; }
+        public string large_image_url { get; set; }
+    }
+
+    public class Webp {
+        public string image_url { get; set; }
+        public string small_image_url { get; set; }
+        public string large_image_url { get; set; }
+    }
+
+    public class Images {
+        public Jpg jpg { get; set; }
+        public Webp webp { get; set; }
+        public object image_url { get; set; }
+        public object small_image_url { get; set; }
+        public object medium_image_url { get; set; }
+        public object large_image_url { get; set; }
+        public object maximum_image_url { get; set; }
+    }
+
+    public class Trailer {
+        public object youtube_id { get; set; }
+        public object url { get; set; }
+        public object embed_url { get; set; }
+        public Images images { get; set; }
+    }
+
+    public class From {
+        public Nullable<int> day { get; set; }
+        public Nullable<int> month { get; set; }
+        public Nullable<int> year { get; set; }
+    }
+
+    public class To {
+        public object day { get; set; }
+        public object month { get; set; }
+        public object year { get; set; }
+    }
+
+    public class Prop {
+        public From from { get; set; }
+        public To to { get; set; }
+    }
+
+    public class Aired {
+        public Nullable<DateTime> from { get; set; }
+        public object to { get; set; }
+        public Prop prop { get; set; }
+        public string @string { get; set; }
+    }
+
+    public class Broadcast {
+        public object day { get; set; }
+        public object time { get; set; }
+        public object timezone { get; set; }
+        public object @string { get; set; }
+    }
+
+    public class Studio {
+        public Nullable<int> mal_id { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Genre {
+        public Nullable<int> mal_id { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Data {
+        public Nullable<int> mal_id { get; set; }
+        public string url { get; set; }
+        public Images images { get; set; }
+        public Trailer trailer { get; set; }
+        public string title { get; set; }
+        public object title_english { get; set; }
+        public string title_japanese { get; set; }
+        public List<string> title_synonyms { get; set; }
+        public string type { get; set; }
+        public string source { get; set; }
+        public Nullable<int> episodes { get; set; }
+        public string status { get; set; }
+        public bool airing { get; set; }
+        public Aired aired { get; set; }
+        public string duration { get; set; }
+        public string rating { get; set; }
+        public object score { get; set; }
+        public object scored_by { get; set; }
+        public Nullable<int> rank { get; set; }
+        public Nullable<int> popularity { get; set; }
+        public Nullable<int> members { get; set; }
+        public Nullable<int> favorites { get; set; }
+        public object synopsis { get; set; }
+        public object background { get; set; }
+        public object season { get; set; }
+        public object year { get; set; }
+        public Broadcast broadcast { get; set; }
+        public List<object> producers { get; set; }
+        public List<object> licensors { get; set; }
+        public List<Studio> studios { get; set; }
+        public List<Genre> genres { get; set; }
+        public List<object> explicit_genres { get; set; }
+        public List<object> themes { get; set; }
+        public List<object> demographics { get; set; }
+    }
+
+    public class Root {
+        public Data data { get; set; }
+    }
+}
