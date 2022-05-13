ad1f71c5edffabc31bd7380344c8240e577f8859
Lukáš Kníže <82168954+LukasKnize@users.noreply.github.com>
2022-05-13 09:36:18 +0200

    Jednoduchý installer pro instalování updatů

---
+++ DesktopWaifuInstaler/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DesktopWaifuInstaler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loadingBarTimer = new System.Windows.Forms.Timer(this.components);
+            this.loaderBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // MainText
+            // 
+            this.MainText.AutoSize = true;
+            this.MainText.ForeColor = System.Drawing.Color.White;
+            this.MainText.Location = new System.Drawing.Point(12, 22);
+            this.MainText.Name = "MainText";
+            this.MainText.Size = new System.Drawing.Size(125, 13);
+            this.MainText.TabIndex = 0;
+            this.MainText.Text = "Updating DeskTopWaifu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please wait, it will take just few seconds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(582, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "If instalation failed, run DesktopWaifuInstaler.exe or contact developers on http" +
+    "s://github.com/LukasKnize/DesktopWaifu";
+            // 
+            // loadingBarTimer
+            // 
+            this.loadingBarTimer.Tick += new System.EventHandler(this.LoadingBarTimer_Tick);
+            // 
+            // loaderBar
+            // 
+            this.loaderBar.Location = new System.Drawing.Point(142, 120);
+            this.loaderBar.Name = "loaderBar";
+            this.loaderBar.Size = new System.Drawing.Size(357, 19);
+            this.loaderBar.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(693, 177);
+            this.ControlBox = false;
+            this.Controls.Add(this.loaderBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer loadingBarTimer;
+        private System.Windows.Forms.ProgressBar loaderBar;
+    }
+}
+
---
+++ DesktopWaifuInstaler/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace DesktopWaifuInstaler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int loaderSteps = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadingBarTimer.Enabled = true;
+            string path = "./DesktopWaifu.exe";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://raw.githubusercontent.com/LukasKnize/DesktopWaifu/productionForInstaler/DesktopWaifu.exe", "DesktopWaifu.exe");
+                }
+            }
+            else
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://raw.githubusercontent.com/LukasKnize/DesktopWaifu/productionForInstaler/DesktopWaifu.exe", "DesktopWaifu.exe");
+                }
+            }
+            
+        }
+
+        private void LoadingBarTimer_Tick(object sender, EventArgs e)
+        {
+            if (loaderSteps < 98)
+            {
+                loaderSteps++;
+            }
+            else
+            {
+                if (File.Exists("./DesktopWaifu.exe"))
+                {
+                    loaderSteps = 100;
+                    loaderBar.Value = loaderSteps;
+                    this.Close();
+                }
+            }
+            
+            loaderBar.Value = loaderSteps;
+        }
+    }
+}
