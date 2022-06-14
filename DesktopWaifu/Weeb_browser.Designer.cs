87d9f695bef0b21ce72744c596bff7509b1755be
Helio <76737930+Heli-o@users.noreply.github.com>
2022-06-14 14:32:33 +0200

    button swap

--- DesktopWaifu/Weeb_browser.Designer.cs
+++ DesktopWaifu/Weeb_browser.Designer.cs
@@ -44,13 +44,14 @@
             // minimizeButton
             // 
             this.minimizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.minimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
+            this.minimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.minimizeButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.minimizeButton.FlatAppearance.BorderSize = 0;
             this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.minimizeButton.Location = new System.Drawing.Point(877, 12);
+            this.minimizeButton.Location = new System.Drawing.Point(1200, 15);
+            this.minimizeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.minimizeButton.Name = "minimizeButton";
-            this.minimizeButton.Size = new System.Drawing.Size(17, 17);
+            this.minimizeButton.Size = new System.Drawing.Size(23, 21);
             this.minimizeButton.TabIndex = 5;
             this.minimizeButton.UseVisualStyleBackColor = false;
             this.minimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
@@ -62,9 +63,10 @@
             this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.closeButton.FlatAppearance.BorderSize = 0;
             this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.closeButton.Location = new System.Drawing.Point(923, 12);
+            this.closeButton.Location = new System.Drawing.Point(1231, 15);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(17, 17);
+            this.closeButton.Size = new System.Drawing.Size(23, 21);
             this.closeButton.TabIndex = 6;
             this.closeButton.UseVisualStyleBackColor = false;
             this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
@@ -83,8 +85,9 @@
             this.navBar.Controls.Add(this.minimizeButton);
             this.navBar.Controls.Add(this.closeButton);
             this.navBar.Location = new System.Drawing.Point(0, 0);
+            this.navBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.navBar.Name = "navBar";
-            this.navBar.Size = new System.Drawing.Size(950, 50);
+            this.navBar.Size = new System.Drawing.Size(1267, 62);
             this.navBar.TabIndex = 8;
             this.navBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NavBar_MouseDown);
             // 
@@ -94,9 +97,10 @@
             this.devTools.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.devTools.ForeColor = System.Drawing.Color.White;
             this.devTools.Image = global::DesktopWaifu.Properties.Resources.tools_fill;
-            this.devTools.Location = new System.Drawing.Point(631, 11);
+            this.devTools.Location = new System.Drawing.Point(841, 14);
+            this.devTools.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.devTools.Name = "devTools";
-            this.devTools.Size = new System.Drawing.Size(44, 24);
+            this.devTools.Size = new System.Drawing.Size(59, 30);
             this.devTools.TabIndex = 13;
             this.devTools.UseVisualStyleBackColor = false;
             this.devTools.Click += new System.EventHandler(this.DevTools_Click);
@@ -104,13 +108,14 @@
             // maximizeButton
             // 
             this.maximizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.maximizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.maximizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
             this.maximizeButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.maximizeButton.FlatAppearance.BorderSize = 0;
             this.maximizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.maximizeButton.Location = new System.Drawing.Point(900, 12);
+            this.maximizeButton.Location = new System.Drawing.Point(1169, 15);
+            this.maximizeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.maximizeButton.Name = "maximizeButton";
-            this.maximizeButton.Size = new System.Drawing.Size(17, 17);
+            this.maximizeButton.Size = new System.Drawing.Size(23, 21);
             this.maximizeButton.TabIndex = 12;
             this.maximizeButton.UseVisualStyleBackColor = false;
             this.maximizeButton.Click += new System.EventHandler(this.MaximizeButton_Click);
@@ -121,9 +126,10 @@
             this.Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Next.ForeColor = System.Drawing.Color.White;
             this.Next.Image = global::DesktopWaifu.Properties.Resources.arrow_go_forward_fill;
-            this.Next.Location = new System.Drawing.Point(53, 11);
+            this.Next.Location = new System.Drawing.Point(71, 14);
+            this.Next.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Next.Name = "Next";
-            this.Next.Size = new System.Drawing.Size(44, 24);
+            this.Next.Size = new System.Drawing.Size(59, 30);
             this.Next.TabIndex = 11;
             this.Next.UseVisualStyleBackColor = false;
             this.Next.Click += new System.EventHandler(this.Next_Click_1);
@@ -134,9 +140,10 @@
             this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Back.ForeColor = System.Drawing.Color.White;
             this.Back.Image = global::DesktopWaifu.Properties.Resources.arrow_go_back_fill1;
-            this.Back.Location = new System.Drawing.Point(3, 11);
+            this.Back.Location = new System.Drawing.Point(4, 14);
+            this.Back.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Back.Name = "Back";
-            this.Back.Size = new System.Drawing.Size(44, 24);
+            this.Back.Size = new System.Drawing.Size(59, 30);
             this.Back.TabIndex = 10;
             this.Back.UseVisualStyleBackColor = false;
             this.Back.Click += new System.EventHandler(this.Back_Click);
@@ -147,13 +154,13 @@
             this.URLInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.URLInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.URLInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(227)))), ((int)(((byte)(230)))));
-            this.URLInput.Location = new System.Drawing.Point(147, 11);
-            this.URLInput.Margin = new System.Windows.Forms.Padding(2);
+            this.URLInput.Location = new System.Drawing.Point(196, 14);
+            this.URLInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.URLInput.MaxLength = 150;
             this.URLInput.Multiline = false;
             this.URLInput.Name = "URLInput";
             this.URLInput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.URLInput.Size = new System.Drawing.Size(388, 24);
+            this.URLInput.Size = new System.Drawing.Size(517, 30);
             this.URLInput.TabIndex = 9;
             this.URLInput.Text = "";
             this.URLInput.WordWrap = false;
@@ -166,9 +173,10 @@
             this.OpenButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.OpenButton.ForeColor = System.Drawing.Color.White;
             this.OpenButton.Image = global::DesktopWaifu.Properties.Resources.search_line;
-            this.OpenButton.Location = new System.Drawing.Point(540, 11);
+            this.OpenButton.Location = new System.Drawing.Point(720, 14);
+            this.OpenButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.OpenButton.Name = "OpenButton";
-            this.OpenButton.Size = new System.Drawing.Size(85, 24);
+            this.OpenButton.Size = new System.Drawing.Size(113, 30);
             this.OpenButton.TabIndex = 8;
             this.OpenButton.UseVisualStyleBackColor = false;
             this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
@@ -179,20 +187,22 @@
             this.WebBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.WebBrowser.Location = new System.Drawing.Point(0, 50);
+            this.WebBrowser.Location = new System.Drawing.Point(0, 62);
+            this.WebBrowser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.WebBrowser.Name = "WebBrowser";
-            this.WebBrowser.Size = new System.Drawing.Size(950, 600);
+            this.WebBrowser.Size = new System.Drawing.Size(1267, 738);
             this.WebBrowser.TabIndex = 9;
             this.WebBrowser.AddressChanged += new System.EventHandler<CefSharp.AddressChangedEventArgs>(this.ChromiumWebBrowser1_AddressChanged);
             // 
             // Weeb_browser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 650);
+            this.ClientSize = new System.Drawing.Size(1267, 800);
             this.Controls.Add(this.WebBrowser);
             this.Controls.Add(this.navBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Weeb_browser";
             this.Text = "Weeb browser";
             this.Load += new System.EventHandler(this.Weeb_browser_Load);
